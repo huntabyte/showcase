2644c4271212823d20c39754c20be2630b7bd6df
engage_intelllect <61082194+engageintellect@users.noreply.github.com>
2023-06-05 18:57:48 -0700

    Update +page.svelte
    responsive project cards for mobile-desktop (sm,md,lg,xl)

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,9 +5,11 @@
 
 <div class="flex flex-col gap-3">
 	<h1 class="text-xl font-bold">Recent Projects</h1>
-	<div class="grid grid-cols-3 gap-2">
-		{#each data.projects as project}
-			<ProjectCard {project} />
-		{/each}
+	<div class="flex justify-center">
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+			{#each data.projects as project}
+				<ProjectCard {project} />
+			{/each}
+		</div>
 	</div>
 </div>
